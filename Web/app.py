5a88b5a09d97e1c61c2ee5fd4ac2aaa47deb1097
JoseTorresMX <joseangel.tosa@outlook.com>
2023-12-02 02:49:42 -0600

    Construyendo Web App con Flask

--- Web/app.py
+++ Web/app.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app =Flask(__name__)
+# Contruccion de la aplicacion
+app = Flask(__name__)
 
+# Rutas que tendra la aplicacion
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__=="__main__":
+
+# Un debuggeador para que, cada vez que se realice cambios
+# se actualize solo
+if __name__ == "__main__":
     app.run(debug=True)
